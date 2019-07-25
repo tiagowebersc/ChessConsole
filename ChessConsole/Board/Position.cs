c555f85204c881c8492a0b52a8b10c4beeb91bdb
Tiago Weber <tiagowebersc@gmail.com>
2019-07-25 19:43:30 +0200

    Created position class

---
+++ ChessConsole/Board/Position.cs
@@ -0,0 +1,19 @@
+﻿namespace Board
+{
+    class Position
+    {
+        public int row { get; set; }
+        public int column { get; set; }
+
+        public Position (int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+
+        public override string ToString()
+        {
+            return row + ", " + column;
+        }
+    }
+}
--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Board;
 
 namespace ChessConsole
 {
@@ -6,7 +7,11 @@ namespace ChessConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Position p;
+            p = new Position(3, 4);
+
+            Console.WriteLine("Position: " + p);
+            Console.ReadLine();
         }
     }
 }
